b5cfd4e62bbd833c720b80a4a638f8263d971ae6
Adam Eivy <atomantic@gmail.com>
2017-03-22 08:04:29 -0700

    not so blazingly fast key repeat

--- install.sh
+++ install.sh
@@ -605,7 +605,7 @@ running "Disable press-and-hold for keys in favor of key repeat"
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false;ok
 
 running "Set a blazingly fast keyboard repeat rate"
-defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain KeyRepeat -int 2
 defaults write NSGlobalDomain InitialKeyRepeat -int 10;ok
 
 running "Set language and text formats (english/US)"
