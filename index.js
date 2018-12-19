c4164fb86b89c55a1948a2e2fc3d570c3baf4853
[._.]/ Adam Eivy <70015+atomantic@users.noreply.github.com>
2018-12-19 11:02:19 -0800

    default gitshots to false, simplify install run
    * only run install on package type if there are configured packages to install
    * default gitshots to false (moving away from this)

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ inquirer.prompt([{
   type: 'confirm',
   name: 'gitshots',
   message: 'Do you want to use gitshots?',
-  default: true
+  default: false
 }]).then(function (answers) {
   if(answers.gitshots){
 
@@ -32,19 +32,16 @@ inquirer.prompt([{
     }
   }
 
-  const installPackages = function(type){
-    console.info(emoji.get('coffee'), ' installing '+type+' packages')
-    config[type].map(function(item){
-      console.info(type+':', item)
-      command('. lib_sh/echos.sh && . lib_sh/requirers.sh && require_'+type+' ' + item, __dirname, function(err, out) {
-        if(err) console.error(emoji.get('fire'), err)
+  ['brew', 'cask', 'npm', 'gem'].forEach( type => {
+    if(config[type] && config[type].length){
+      console.info(emoji.get('coffee'), ' installing '+type+' packages')
+      config[type].map(function(item){
+        console.info(type+':', item)
+        command('. lib_sh/echos.sh && . lib_sh/requirers.sh && require_'+type+' ' + item, __dirname, function(err, out) {
+          if(err) console.error(emoji.get('fire'), err)
+        })
       })
-    })
-  }
-
-  installPackages('brew')
-  installPackages('cask')
-  installPackages('npm')
-  installPackages('gem')
+    }
+  })
 
 })
