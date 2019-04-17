913dd623214fceb8012e7895ba35fe2c374ae393
[._.]/ Adam Eivy <70015+atomantic@users.noreply.github.com>
2019-04-17 13:55:34 -0700

    autojump

--- config.js
+++ config.js
@@ -3,6 +3,8 @@ module.exports = {
     // http://conqueringthecommandline.com/book/ack_ag
     'ack',
     'ag',
+    // https://github.com/wting/autojump
+    'autojump',
     // alternative to `cat`: https://github.com/sharkdp/bat
     'bat',
     // Install GNU core utilities (those that come with macOS are outdated)
