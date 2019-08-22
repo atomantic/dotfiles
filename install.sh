73ed406dc2e2251a83dbb6c84f0dd1496e75ece8
Paul Nodet <paul.nodet@gmail.com>
2019-08-22 11:51:49 +0200

    nicer ?

--- install.sh
+++ install.sh
@@ -154,10 +154,29 @@ fi
 # ###########################################################
 # Install non-brew various tools (PRE-BREW Installs)
 # ###########################################################
+
 bot "ensuring build/install tools are available"
-xcode-select --install 2>&1 > /dev/null
-sudo xcode-select -s /Applications/Xcode.app/Contents/Developer 2>&1 > /dev/null
-sudo xcodebuild -license accept 2>&1 > /dev/null
+if ! xcode-select --print-path &> /dev/null; then
+
+    # Prompt user to install the XCode Command Line Tools
+    xcode-select --install &> /dev/null
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Wait until the XCode Command Line Tools are installed
+    until xcode-select --print-path &> /dev/null; do
+        sleep 5
+    done
+
+    print_result $? ' XCode Command Line Tools Installed'
+
+    # Prompt user to agree to the terms of the Xcode license
+    # https://github.com/alrra/dotfiles/issues/10
+
+    sudo xcodebuild -license
+    print_result $? 'Agree with the XCode Command Line Tools licence'
+
+fi
 
 # ###########################################################
 # install homebrew (CLI Packages)
