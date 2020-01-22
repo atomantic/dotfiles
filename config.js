a40f13738e3906a92c7932e7bed9fd855917ebec
Matt C. Wilson <matt-wilson@teletracking.com>
2020-01-22 00:00:11 -0500

    Add mas as an app install type in config.js

--- config.js
+++ config.js
@@ -93,5 +93,17 @@ module.exports = {
     'trash',
     'vtop'
     // ,'yo'
-  ]
+  ],
+  mas: [
+    //com.if.Amphetamine (4.1.6)
+    '937984704',
+    //net.shinyfrog.bear (1.6.15)
+    '1091189122',
+    //com.monosnap.monosnap (3.5.8)
+    '540348655',
+    //com.app77.pwsafemac (4.17)
+    '520993579',
+    //com.apple.dt.Xcode (10.2.1)
+    '497799835',
+  ],
 };
