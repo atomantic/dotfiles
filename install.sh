0f6581944619ac083c40e80af27515ab050f029b
Ilya Eivy <65735275+fiercefairy@users.noreply.github.com>
2023-07-26 09:49:50 -0700

    add brew to path after install

--- install.sh
+++ install.sh
@@ -192,6 +192,8 @@ if [[ $? != 0 ]]; then
     error "unable to install homebrew, script $0 abort!"
     exit 2
   fi
+  (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/${whoami}/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
   brew analytics off
 else
   ok
