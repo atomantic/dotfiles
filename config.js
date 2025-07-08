d49f6b3fd1337d6a3638ea18ab9da1fdfaa61c81
[._.]/ Adam Eivy <atomantic@gmail.com>
2025-07-08 09:14:52 -0700

    fix gemini-cli install

--- config.js
+++ config.js
@@ -1,54 +1,54 @@
 module.exports = {
   brew: [
     // http://conqueringthecommandline.com/book/ack_ag
-    'ack',
-    'ag',
+    "ack",
+    "ag",
     // https://github.com/wting/autojump
-    'autojump',
+    "autojump",
     // alternative to `cat`: https://github.com/sharkdp/bat
-    'bat',
+    "bat",
     // Install GNU core utilities (those that come with macOS are outdated)
     // Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-    'coreutils',
-    'dos2unix',
+    "coreutils",
+    "dos2unix",
     // Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
-    'findutils',
-    'fortune',
-    'fzf',
-    'readline', // ensure gawk gets good readline
-    'gawk',
+    "findutils",
+    "fortune",
+    "fzf",
+    "readline", // ensure gawk gets good readline
+    "gawk",
     // http://www.lcdf.org/gifsicle/ (because I'm a gif junky)
-    'gifsicle',
-    'gnupg',
+    "gifsicle",
+    "gnupg",
     // Install GNU `sed`, overwriting the built-in `sed`
     // so we can do "sed -i 's/foo/bar/' file" instead of "sed -i '' 's/foo/bar/' file"
-    'gnu-sed --with-default-names',
+    "gnu-sed --with-default-names",
     // upgrade grep so we can get things like inverted match (-v)
-    'grep --with-default-names',
+    "grep --with-default-names",
     // better, more recent grep
-    'homebrew/dupes/grep',
+    "homebrew/dupes/grep",
     // https://github.com/jkbrzt/httpie
-    'httpie',
+    "httpie",
     // jq is a sort of JSON grep
-    'jq',
+    "jq",
     // Mac App Store CLI: https://github.com/mas-cli/mas
-    'mas',
+    "mas",
     // Install some other useful utilities like `sponge`
-    'moreutils',
-    'nmap',
+    "moreutils",
+    "nmap",
     // 'openconnect',
-    'reattach-to-user-namespace',
+    "reattach-to-user-namespace",
     // better/more recent version of screen
-    'homebrew/dupes/screen',
-    'tmux',
-    'todo-txt',
-    'tree',
-    'ttyrec',
+    "homebrew/dupes/screen",
+    "tmux",
+    "todo-txt",
+    "tree",
+    "ttyrec",
     // better, more recent vim
-    'vim --with-client-server --with-override-system-vi',
-    'watch',
+    "vim --with-client-server --with-override-system-vi",
+    "watch",
     // Install wget with IRI support
-    'wget --enable-iri'
+    "wget --enable-iri",
   ],
   cask: [
     //'adium',
@@ -57,42 +57,41 @@ module.exports = {
     //'box-sync',
     //'comicbooklover',
     //'diffmerge',
-    'docker', // docker for mac
+    "docker", // docker for mac
     //'dropbox',
     //'evernote',
     //'flux',
-    'gpg-suite',
+    "gpg-suite",
     //'ireadfast',
-    'iterm2',
+    "iterm2",
     //'little-snitch',
     // 'macbreakz',
     //'micro-snitch',
     // 'signal',
     //'macvim',
-    'sizeup',
+    "sizeup",
     //'sketchup',
-    'slack',
+    "slack",
     // 'the-unarchiver',
     //'torbrowser',
     //'transmission',
-    'visual-studio-code',
+    "visual-studio-code",
     //'vlc',
-    'xquartz'
+    "xquartz",
   ],
-  gem: [
-  ],
+  gem: [],
   npm: [
-    '@anthropic-ai/claude-code',
-    'antic',
-    'buzzphrase',
-    'eslint',
-    'gemini-cli',
-    'trash',
-    'vtop'
+    "@anthropic-ai/claude-code",
+    "antic",
+    "buzzphrase",
+    "eslint",
+    "@google/gemini-cli",
+    "trash",
+    "vtop",
   ],
   mas: [
     //com.apple.dt.Xcode (10.2.1)
-    '497799835',
+    "497799835",
     //com.if.Amphetamine (4.1.6)
     //'937984704',
     //net.shinyfrog.bear (1.6.15)
