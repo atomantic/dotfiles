84229c2fa6972f83c6f7d3046bcd5258953c737b
[._.]/ Adam Eivy <atomantic@gmail.com>
2025-07-08 11:07:43 -0700

    wrap gitconfig in a prompt to allow skipping on repeat runs

--- install.sh
+++ install.sh
@@ -58,74 +58,79 @@ fi
 # Git Config
 # ###########################################################
 bot "OK, now I am going to update the .gitconfig for your user info:"
-grep 'user = GITHUBUSER' ./homedir/.gitconfig > /dev/null 2>&1
-if [[ $? = 0 ]]; then
-    read -r -p "What is your git username? " githubuser
+read -r -p "Do you want to update your .gitconfig user info? [y|N] " update_gitconfig_response
+if [[ $update_gitconfig_response =~ (yes|y|Y) ]]; then
+  grep 'user = GITHUBUSER' ./homedir/.gitconfig > /dev/null 2>&1
+  if [[ $? = 0 ]]; then
+      read -r -p "What is your git username? " githubuser
 
-  fullname=`osascript -e "long user name of (system info)"`
+    fullname=`osascript -e "long user name of (system info)"`
 
-  if [[ -n "$fullname" ]];then
-    lastname=$(echo $fullname | awk '{print $2}');
-    firstname=$(echo $fullname | awk '{print $1}');
-  fi
+    if [[ -n "$fullname" ]];then
+      lastname=$(echo $fullname | awk '{print $2}');
+      firstname=$(echo $fullname | awk '{print $1}');
+    fi
 
-  if [[ -z $lastname ]]; then
-    lastname=`dscl . -read /Users/$(whoami) | grep LastName | sed "s/LastName: //"`
-  fi
-  if [[ -z $firstname ]]; then
-    firstname=`dscl . -read /Users/$(whoami) | grep FirstName | sed "s/FirstName: //"`
-  fi
-  email=`dscl . -read /Users/$(whoami)  | grep EMailAddress | sed "s/EMailAddress: //"`
+    if [[ -z $lastname ]]; then
+      lastname=`dscl . -read /Users/$(whoami) | grep LastName | sed "s/LastName: //"`
+    fi
+    if [[ -z $firstname ]]; then
+      firstname=`dscl . -read /Users/$(whoami) | grep FirstName | sed "s/FirstName: //"`
+    fi
+    email=`dscl . -read /Users/$(whoami)  | grep EMailAddress | sed "s/EMailAddress: //"`
 
-  if [[ ! "$firstname" ]]; then
-    response='n'
-  else
-    echo -e "I see that your full name is $COL_YELLOW$firstname $lastname$COL_RESET"
-    read -r -p "Is this correct? [Y|n] " response
-  fi
+    if [[ ! "$firstname" ]]; then
+      response='n'
+    else
+      echo -e "I see that your full name is $COL_YELLOW$firstname $lastname$COL_RESET"
+      read -r -p "Is this correct? [Y|n] " response
+    fi
 
-  if [[ $response =~ ^(no|n|N) ]]; then
-    read -r -p "What is your first name? " firstname
-    read -r -p "What is your last name? " lastname
-  fi
-  fullname="$firstname $lastname"
+    if [[ $response =~ ^(no|n|N) ]]; then
+      read -r -p "What is your first name? " firstname
+      read -r -p "What is your last name? " lastname
+    fi
+    fullname="$firstname $lastname"
 
-  bot "Great $fullname, "
+    bot "Great $fullname, "
 
-  if [[ ! $email ]]; then
-    response='n'
-  else
-    echo -e "The best I can make out, your email address is $COL_YELLOW$email$COL_RESET"
-    read -r -p "Is this correct? [Y|n] " response
-  fi
+    if [[ ! $email ]]; then
+      response='n'
+    else
+      echo -e "The best I can make out, your email address is $COL_YELLOW$email$COL_RESET"
+      read -r -p "Is this correct? [Y|n] " response
+    fi
 
-  if [[ $response =~ ^(no|n|N) ]]; then
-    read -r -p "What is your email? " email
-    if [[ ! $email ]];then
-      error "you must provide an email to configure .gitconfig"
-      exit 1
+    if [[ $response =~ ^(no|n|N) ]]; then
+      read -r -p "What is your email? " email
+      if [[ ! $email ]];then
+        error "you must provide an email to configure .gitconfig"
+        exit 1
+      fi
+    fi
+
+
+    running "replacing items in .gitconfig with your info ($COL_YELLOW$fullname, $email, $githubuser$COL_RESET)"
+
+    # test if gnu-sed or MacOS sed
+
+    sed -i "s/GITHUBFULLNAME/$firstname $lastname/" ./homedir/.gitconfig > /dev/null 2>&1 || true
+    if [[ ${PIPESTATUS[0]} != 0 ]]; then
+      echo
+      running "looks like you are using MacOS sed rather than gnu-sed, accommodating"
+      sed -i '' "s/GITHUBFULLNAME/$firstname $lastname/" ./homedir/.gitconfig
+      sed -i '' 's/GITHUBEMAIL/'$email'/' ./homedir/.gitconfig
+      sed -i '' 's/GITHUBUSER/'$githubuser'/' ./homedir/.gitconfig
+      ok
+    else
+      echo
+      bot "looks like you are already using gnu-sed. woot!"
+      sed -i 's/GITHUBEMAIL/'$email'/' ./homedir/.gitconfig
+      sed -i 's/GITHUBUSER/'$githubuser'/' ./homedir/.gitconfig
     fi
   fi
-
-
-  running "replacing items in .gitconfig with your info ($COL_YELLOW$fullname, $email, $githubuser$COL_RESET)"
-
-  # test if gnu-sed or MacOS sed
-
-  sed -i "s/GITHUBFULLNAME/$firstname $lastname/" ./homedir/.gitconfig > /dev/null 2>&1 || true
-  if [[ ${PIPESTATUS[0]} != 0 ]]; then
-    echo
-    running "looks like you are using MacOS sed rather than gnu-sed, accommodating"
-    sed -i '' "s/GITHUBFULLNAME/$firstname $lastname/" ./homedir/.gitconfig
-    sed -i '' 's/GITHUBEMAIL/'$email'/' ./homedir/.gitconfig
-    sed -i '' 's/GITHUBUSER/'$githubuser'/' ./homedir/.gitconfig
-    ok
-  else
-    echo
-    bot "looks like you are already using gnu-sed. woot!"
-    sed -i 's/GITHUBEMAIL/'$email'/' ./homedir/.gitconfig
-    sed -i 's/GITHUBUSER/'$githubuser'/' ./homedir/.gitconfig
-  fi
+else
+  ok "skipped .gitconfig user info update.";
 fi
 
 # ###########################################################
