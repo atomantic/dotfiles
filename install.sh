20c0e1152a13750fb99d3e7ed1fc3148bc3ca716
[._.]/ Adam Eivy <atomantic@gmail.com>
2025-07-08 15:58:10 -0700

    use iterm2 plist for configs

--- install.sh
+++ install.sh
@@ -963,60 +963,12 @@ sudo mdutil -i on / > /dev/null;ok
 bot "Terminal & iTerm2"
 ###############################################################################
 
-# running "Only use UTF-8 in Terminal.app"
-# defaults write com.apple.terminal StringEncodings -array 4;ok
-#
-# running "Use a modified version of the Solarized Dark theme by default in Terminal.app"
-# TERM_PROFILE='Solarized Dark xterm-256color';
-# CURRENT_PROFILE="$(defaults read com.apple.terminal 'Default Window Settings')";
-# if [ "${CURRENT_PROFILE}" != "${TERM_PROFILE}" ]; then
-# 	open "./configs/${TERM_PROFILE}.terminal";
-# 	sleep 1; # Wait a bit to make sure the theme is loaded
-# 	defaults write com.apple.terminal 'Default Window Settings' -string "${TERM_PROFILE}";
-# 	defaults write com.apple.terminal 'Startup Window Settings' -string "${TERM_PROFILE}";
-# fi;
-
-#running "Enable “focus follows mouse” for Terminal.app and all X11 apps"
-# i.e. hover over a window and start `typing in it without clicking first
-defaults write com.apple.terminal FocusFollowsMouse -bool true
-#defaults write org.x.X11 wm_ffm -bool true;ok
-
-running "Installing the Solarized Light theme for iTerm (opening file)"
-open "./configs/Solarized Light.itermcolors";ok
-running "Installing the Patched Solarized Dark theme for iTerm (opening file)"
-open "./configs/Solarized Dark Patch.itermcolors";ok
-
 # Import iTerm2 profile
-if [ -f "./configs/iTerm2.itermexport" ]; then
-  running "Importing iTerm2 profile from iTerm2.itermexport"
-  open "./configs/iTerm2.itermexport"
-  ok
-  bot "iTerm2 profile imported. Please restart iTerm2 to apply all settings."
-fi
-
-running "Don’t display the annoying prompt when quitting iTerm"
-defaults write com.googlecode.iterm2 PromptOnQuit -bool false;ok
-running "hide tab title bars"
-defaults write com.googlecode.iterm2 HideTab -bool true;ok
-running "set system-wide hotkey to show/hide iterm with ^\`"
-defaults write com.googlecode.iterm2 Hotkey -bool true;ok
-running "hide pane titles in split panes"
-defaults write com.googlecode.iterm2 ShowPaneTitles -bool false;ok
-running "animate split-terminal dimming"
-defaults write com.googlecode.iterm2 AnimateDimming -bool true;ok
-#defaults write com.googlecode.iterm2 HotkeyChar -int 96;
-#defaults write com.googlecode.iterm2 HotkeyCode -int 50;
-#defaults write com.googlecode.iterm2 FocusFollowsMouse -int 1;
-#defaults write com.googlecode.iterm2 HotkeyModifiers -int 262401;
-running "Make iTerm2 load new tabs in the same directory"
-/usr/libexec/PlistBuddy -c "set \"New Bookmarks\":0:\"Custom Directory\" Recycle" ~/Library/Preferences/com.googlecode.iterm2.plist
-running "setting fonts"
-defaults write com.googlecode.iterm2 "Normal Font" -string "Hack-Regular 12";
-defaults write com.googlecode.iterm2 "Non Ascii Font" -string "RobotoMonoForPowerline-Regular 12";
+running "Importing iTerm2 profile from com.googlecode.iterm2.plist"
+defaults write com.googlecode.iterm2 PrefsCustomFolder -string "$HOME/.dotfiles/configs"
+defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
 ok
-running "reading iterm settings"
-defaults read -app iTerm > /dev/null 2>&1;
-ok
+bot "iTerm2 profile imported. Please restart iTerm2 to apply all settings."
 
 ###############################################################################
 bot "Time Machine"
