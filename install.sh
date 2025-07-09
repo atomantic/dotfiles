8496ea24519f3fe16b412d56e44dc36b52db39ff
Marc Schärer <dreamora@users.noreply.github.com>
2025-07-09 17:13:18 +0200

    . R: Migrate symlink management to stow

--- install.sh
+++ install.sh
@@ -244,32 +244,8 @@ fi
 p10k configure
 
 bot "Dotfiles Setup"
-read -r -p "symlink ./homedir/* files in ~/ (these are the dotfiles)? [y|N] " response
-if [[ $response =~ (y|yes|Y) ]]; then
-  bot "creating symlinks for project dotfiles..."
-  pushd homedir >/dev/null 2>&1
-  now=$(date +"%Y.%m.%d.%H.%M.%S")
-
-  for file in .*; do
-    if [[ $file == "." || $file == ".." ]]; then
-      continue
-    fi
-    running "~/$file"
-    # if the file exists:
-    if [[ -e ~/$file ]]; then
-      mkdir -p ~/.dotfiles_backup/$now
-      mv ~/$file ~/.dotfiles_backup/$now/$file
-      echo "backup saved as ~/.dotfiles_backup/$now/$file"
-    fi
-    # symlink might still exist
-    unlink ~/$file >/dev/null 2>&1
-    # create the link
-    ln -s ~/.dotfiles/homedir/$file ~/$file
-    echo -en '\tlinked'
-    ok
-  done
-  popd >/dev/null 2>&1
-fi
+bot "symlinking homedir dotfiles with GNU stow"
+stow -v -d "$HOME/.dotfiles" -t "$HOME" homedir
 
 bot "create symlink for nvim"
 ln -s ~/.dotfiles/nvim ~/.config/nvim
