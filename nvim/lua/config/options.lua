8814bf0a48967505f003df7770014744158e4480
Marc Schärer <dreamora@users.noreply.github.com>
2025-07-09 16:55:43 +0200

    . R: Update neovim dependencies and remove unneeded menu item

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -21,5 +21,4 @@ vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.wo.number = true
