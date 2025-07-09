68cfc0416225fa2e4fa3731ef424205934fdbe63
Marc Schärer <dreamora@users.noreply.github.com>
2025-07-09 23:53:15 +0200

    . R: Improve navbuddy setup

--- nvim/lua/plugins/navigation.lua
+++ nvim/lua/plugins/navigation.lua
@@ -34,39 +34,11 @@ return {
           'SmiteshP/nvim-navic',
           'MunifTanjim/nui.nvim',
         },
-        opts = { lazy = false, lsp = { auto_attach = true } },
+        opts = { lazy = true, lsp = { auto_attach = true } },
         keys = {
           { '<leader>cn', '<cmd>Navbuddy<CR>', { desc = 'Open Navbuddy' } },
         },
-        -- enabling the configuration somehow breaks the logic, with and without the keymapj
-        -- config = function()
-        -- vim.keymap.set('n', '<leader>cn', ':Navbuddy<CR>', { desc = 'Open Navbuddy' })
-        --   local navbuddy = require('nvim-navbuddy')
-        --   local actions = require('nvim-navbuddy.actions')
-        --
-        --   navbuddy.setup({
-        --     use_default_mappings = true,
-        --   })
-        -- end,
       },
     },
   },
-  -- {
-  --   'SmiteshP/nvim-navic',
-  --   config = function()
-  --     local navic = require('nvim-navic')
-  --     navic.setup({
-  --       highlight = true,
-  --       lsp = {
-  --         auto_attach = true,
-  --       },
-  --     })
-  --   end,
-  -- },
-  --   {
-  --     'rebelot/heirline.nvim',
-  --     config = function()
-  --       require('heirline').setup({})
-  --     end,
-  --   },
 }
