bf21c098637d00abe978be2146258db57a7bf1d9
Marc Schärer <dreamora@users.noreply.github.com>
2025-07-13 17:01:35 +0200

    . R: Improve requirers to use zsh instead of bash rc

--- lib_sh/requirers.sh
+++ lib_sh/requirers.sh
@@ -8,98 +8,98 @@
 # source ./echos.sh
 
 function require_apm() {
-    running "checking atom plugin: $1"
-    apm list --installed --bare | grep $1@ > /dev/null
-    if [[ $? != 0 ]]; then
-        action "apm install $1"
-        apm install $1
-    fi
-    ok
+  running "checking atom plugin: $1"
+  apm list --installed --bare | grep $1@ >/dev/null
+  if [[ $? != 0 ]]; then
+    action "apm install $1"
+    apm install $1
+  fi
+  ok
 }
 
 function require_brew() {
-    running "brew $1 $2"
-    brew list $1 > /dev/null 2>&1 | true
-    if [[ ${PIPESTATUS[0]} != 0 ]]; then
-        action "brew install $1 $2"
-        brew install $1 $2
-        if [[ $? != 0 ]]; then
-            error "failed to install $1! aborting..."
-            # exit -1
-        fi
+  running "brew $1 $2"
+  brew list $1 >/dev/null 2>&1 | true
+  if [[ ${PIPESTATUS[0]} != 0 ]]; then
+    action "brew install $1 $2"
+    brew install $1 $2
+    if [[ $? != 0 ]]; then
+      error "failed to install $1! aborting..."
+      # exit -1
     fi
-    ok
+  fi
+  ok
 }
 
 function require_cask() {
-    running "brew cask $1"
-    brew cask list $1 > /dev/null 2>&1 | true
-    if [[ ${PIPESTATUS[0]} != 0 ]]; then
-        action "brew cask install $1 $2"
-        brew install --cask $1
-        if [[ $? != 0 ]]; then
-            error "failed to install $1! aborting..."
-            # exit -1
-        fi
+  running "brew cask $1"
+  brew cask list $1 >/dev/null 2>&1 | true
+  if [[ ${PIPESTATUS[0]} != 0 ]]; then
+    action "brew cask install $1 $2"
+    brew install --cask $1
+    if [[ $? != 0 ]]; then
+      error "failed to install $1! aborting..."
+      # exit -1
     fi
-    ok
+  fi
+  ok
 }
 
 function require_gem() {
-    running "gem $1"
-    if [[ $(gem list --local | grep $1 | head -1 | cut -d' ' -f1) != $1 ]]; then
-        action "gem install $1"
-        gem install $1
-    fi
-    ok
+  running "gem $1"
+  if [[ $(gem list --local | grep $1 | head -1 | cut -d' ' -f1) != $1 ]]; then
+    action "gem install $1"
+    gem install $1
+  fi
+  ok
 }
 
 function require_mas() {
-    running "mas $1"
-    if [[ $(mas list | grep $1 | head -1 | cut -d' ' -f1) != $1 ]]; then
-        action "mas install $1"
-        mas install $1
-    fi
-    ok
+  running "mas $1"
+  if [[ $(mas list | grep $1 | head -1 | cut -d' ' -f1) != $1 ]]; then
+    action "mas install $1"
+    mas install $1
+  fi
+  ok
 }
 
-function require_node(){
-    running "node -v"
-    node -v
-    if [[ $? != 0 ]]; then
-        action "node not found, installing via homebrew"
-        brew install node
-    fi
-    ok
+function require_node() {
+  running "node -v"
+  node -v
+  if [[ $? != 0 ]]; then
+    action "node not found, installing via homebrew"
+    brew install node
+  fi
+  ok
 }
 
 function require_npm() {
-    sourceNVM
-    nvm use stable
-    running "npm $*"
-    npm list -g --depth 0 | grep $1@ > /dev/null
-    if [[ $? != 0 ]]; then
-        action "npm install -g $*"
-        npm install -g $@
-    fi
-    ok
+  sourceNVM
+  nvm use stable
+  running "npm $*"
+  npm list -g --depth 0 | grep $1@ >/dev/null
+  if [[ $? != 0 ]]; then
+    action "npm install -g $*"
+    npm install -g $@
+  fi
+  ok
 }
 
-function sourceNVM(){
-    export NVM_DIR=~/.nvm
-    source $(brew --prefix nvm)/nvm.sh
+function sourceNVM() {
+  export NVM_DIR=~/.nvm
+  source $(brew --prefix nvm)/nvm.sh
 }
 
 function require_nvm() {
-    mkdir -p ~/.nvm
-    cp $(brew --prefix nvm)/nvm-exec ~/.nvm/
-    sourceNVM
+  mkdir -p ~/.nvm
+  cp $(brew --prefix nvm)/nvm-exec ~/.nvm/
+  sourceNVM
+  nvm install $1
+  if [[ $? != 0 ]]; then
+    action "installing nvm"
+    require_brew nvm
+    . ~/.zshrc
     nvm install $1
-    if [[ $? != 0 ]]; then
-        action "installing nvm"
-        require_brew nvm
-        . ~/.bashrc
-        nvm install $1
-    fi
-    ok
+  fi
+  ok
 }
