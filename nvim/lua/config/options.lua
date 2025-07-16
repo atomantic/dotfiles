bb744f0d5633a9aacd14fb7f32e4e7e2978594e1
Marc Schärer <dreamora@users.noreply.github.com>
2025-07-16 03:33:49 +0200

    e: Disable debug level lsp logging

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,7 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 --
-vim.lsp.set_log_level('debug')
+-- vim.lsp.set_log_level('debug')
+vim.lsp.set_log_level('off')
 
 -- my own configs
 -- vim.cmd('set autochdir=true')
